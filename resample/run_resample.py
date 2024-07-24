76ead845834c90d8e82ca246ac13a8cb4c10a9a7
GaelleLeroux <gaelleleroux974@gmail.com>
2024-07-24 09:06:09 -0400

    STYLE : test input

--- resample/run_resample.py
+++ resample/run_resample.py
@@ -21,9 +21,9 @@ def main(args):
             out_path = row["out"]
             print(size[1])
             print(f'Image d\'entrée: {row["in"]}, Image de sortie: {row["out"]}, Taille: {size}')
-            # command = [f"python3 {args.python_file} --img \"{input_path}\" --out \"{out_path}\" --size 768 576 768 --spacing 0.3 0.3 0.3 --center False --linear False --fit_spacing True --image_dimension 3 --pixel_dimension 1 --rgb False --ow 0"]
-            # command = [f"python3 {args.python_file} --img \"{input_path}\" --out \"{out_path}\" --size 443 443 119 --spacing 0.3 0.3 0.3 --center False --linear False --fit_spacing True --image_dimension 3 --pixel_dimension 1 --rgb False --ow 0"]
-            command = [f"python3 {args.python_file} --img \"{input_path}\" --out \"{out_path}\" --size 443 443 443  --fit_spacing True --center 0 --iso_spacing 1 --linear False --image_dimension 3 --pixel_dimension 1 --rgb False --ow 0"]
+            # command = [f"python3 {args.python_file} --img \"{input_path}\" --out \"{out_path}\" --size 768 576 768 --spacing 0.3 0.3 0.3 --center 0 --linear False --fit_spacing True --image_dimension 3 --pixel_dimension 1 --rgb False --ow 0"]
+            # command = [f"python3 {args.python_file} --img \"{input_path}\" --out \"{out_path}\" --size 443 443 119 --spacing 0.3 0.3 0.3 --center 0 --linear False --fit_spacing True --image_dimension 3 --pixel_dimension 1 --rgb False --ow 0"]
+            command = [f"python3 {args.python_file} --img \"{input_path}\" --out \"{out_path}\" --size 119 443 443  --fit_spacing False --center 0 --linear False --image_dimension 3 --pixel_dimension 1 --rgb False --ow 0"]
             subprocess.run(command,shell=True)
             
 
@@ -32,7 +32,7 @@ if __name__=="__main__":
     # SIZE AND SPACING TO RESAMPLE ARE HARD WRITTEN IN THE LINE 24
     parser = argparse.ArgumentParser(description='Get nifti info')
     parser.add_argument('--csv', required=True,type=str, help='name of the CSV')
-    parser.add_argument('--python_file', default="/home/luciacev/Documents/Gaelle/MultimodelRegistration/resample/resample.py",type=str, help='path to resample.py')
+    parser.add_argument('--python_file', default="/home/lucia/Documents/Gaelle/MultimodelRegistration/resample/resample.py",type=str, help='path to resample.py')
     # /home/luciacev/Documents/Gaelle/MultimodelRegistration/resample/resample.py
     args = parser.parse_args()
 
